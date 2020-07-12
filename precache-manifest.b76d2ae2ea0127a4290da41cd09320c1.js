86f97c546084c0ff728fd1d1f79e34cd5f3750ec
Narain Sagar <narainmenghwar@gmail.com>
2020-07-12 18:55:58 +0200

    Updates

---
+++ precache-manifest.b76d2ae2ea0127a4290da41cd09320c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13a9b8ec2e9aeebcb23716499759f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b1a3dce7bdcf7df938",
+    "url": "/static/css/main.8913ef21.chunk.css"
+  },
+  {
+    "revision": "6e3ec7b1b71fbab27e64",
+    "url": "/static/js/2.af1d83c1.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.af1d83c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b1a3dce7bdcf7df938",
+    "url": "/static/js/main.f4eaaad5.chunk.js"
+  },
+  {
+    "revision": "a4508c6334ddf460f57e",
+    "url": "/static/js/runtime-main.ea96fe3e.js"
+  }
+]);
