f4548efa5d150bd629fda3306b3de6b13931a435
Narain Sagar <narainmenghwar@gmail.com>
2020-07-12 19:04:53 +0200

    Updates

---
+++ precache-manifest.1aab705415b212cd5c4ad55ee69eca63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346067b7f1598bfd038294970a748428",
+    "url": "/swapi-starships-app/index.html"
+  },
+  {
+    "revision": "b5b1a3dce7bdcf7df938",
+    "url": "/swapi-starships-app/static/css/main.8913ef21.chunk.css"
+  },
+  {
+    "revision": "6e3ec7b1b71fbab27e64",
+    "url": "/swapi-starships-app/static/js/2.af1d83c1.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/swapi-starships-app/static/js/2.af1d83c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b1a3dce7bdcf7df938",
+    "url": "/swapi-starships-app/static/js/main.f4eaaad5.chunk.js"
+  },
+  {
+    "revision": "72ecfcb4af9cbd1ef924",
+    "url": "/swapi-starships-app/static/js/runtime-main.8276d863.js"
+  }
+]);
